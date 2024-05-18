a87d089441a30b0b1e95dbae661736a2db917f4c
FatimaBouhanou <84349819+FatimaBouhanou@users.noreply.github.com>
2024-05-18 17:57:27 +0100

    pushing the project into git

---
+++ src/main/java/emsi/IIR4/ReservationProject/ReservationProjectApplication.java
@@ -0,0 +1,13 @@
+package emsi.IIR4.ReservationProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/emsi/IIR4/ReservationProject/ReservationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package emsi.IIR4.ReservationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
